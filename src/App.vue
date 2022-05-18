5c09c8c2d8a3827aace891b2bb8ae73f2cd39322
tohsaka888 <156132264@qq.com>
2022-05-18 10:30:11 +0800

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <div v-for="item in comments" :key="item.id"> -->
+  <HelloWorld :comments="comments" />
+  <!-- </div> -->
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+import data from "./data.json";
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
+  data() {
+    return {
+      comments: data,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="comments.length !== 0">
+    <div v-for="item in comments" :key="item.id" class="comment-item">
+      {{ "name:" + item.name + "    id:" + item.id }}
+      <HelloWorld :comments="item.children" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    comments: [],
+  },
+  created() {
+    console.log(this.comments);
+    console.log(this.comments.length);
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.comment-item {
+  margin-left: 15px;
+  text-align: left;
+}
+</style>
